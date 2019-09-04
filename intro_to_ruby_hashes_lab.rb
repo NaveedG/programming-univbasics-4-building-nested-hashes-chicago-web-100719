d9f1b05586edbcdf909eb33212fbcd0ae5b6c467
Naveed Ganjani <nganjani@protonmail.com>
2019-09-04 16:50:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,6 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-]
 { :railroads => { pieces: 4,
                     :rent_in_dollars => { one_piece_owned: 25,
                                           two_pieces_owned: 50,
